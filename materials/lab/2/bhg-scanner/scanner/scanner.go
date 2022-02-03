eaf4f6a5bcccef3893d346038eab33bebc94b055
areiche1 <areiche1@uwyo.edu>
2022-02-02 22:29:22 -0500

    Implemented TODOs in scanner.go for #4

--- materials/lab/2/bhg-scanner/scanner/scanner.go
+++ materials/lab/2/bhg-scanner/scanner/scanner.go
@@ -1,7 +1,10 @@
 // bhg-scanner/scanner.go modified from Black Hat Go > CH2 > tcp-scanner-final > main.go
 // Code : https://github.com/blackhat-go/bhg/blob/c27347f6f9019c8911547d6fc912aa1171e6c362/ch-2/tcp-scanner-final/main.go
 // License: {$RepoRoot}/materials/BHG-LICENSE
-// Useage:
+// Useage: Scans ports 1-1024 of scanme.nmap.org and reports which ones are open and which are closed
+// Adeline Reichert
+// 2/3/22
+// COSC 4010-01 Lab 2
 // {TODO 1: FILL IN}
 
 package scanner
@@ -10,18 +13,22 @@ import (
 	"fmt"
 	"net"
 	"sort"
+	"time"
 )
 
 //TODO 3 : ADD closed ports; currently code only tracks open ports
 var openports []int  // notice the capitalization here. access limited!
+var closedports []int
 
 
 func worker(ports, results chan int) {
 	for p := range ports {
 		address := fmt.Sprintf("scanme.nmap.org:%d", p)    
-		conn, err := net.Dial("tcp", address) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
+		// conn, err := net.Dial("tcp", address) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
+		conn, err := net.DialTimeout("tcp", address, 1 * time.Second)
 		if err != nil { 
 			results <- 0
+			closedports = append(closedports, p)
 			continue
 		}
 		conn.Close()
@@ -34,9 +41,9 @@ func worker(ports, results chan int) {
 // med: easy + return  complex data structure(s?) (maps or slices) containing the ports.
 // hard: restructuring code - consider modification to class/object 
 // No matter what you do, modify scanner_test.go to align; note the single test currently fails
-func PortScanner() int {  
-
-	ports := make(chan int, 100)   // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
+func PortScanner() (int, int) {  
+	start := time.Now()	// To help tune for TODO 4
+	ports := make(chan int, 1024)   // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
 	results := make(chan int)
 
 	for i := 0; i < cap(ports); i++ {
@@ -59,13 +66,19 @@ func PortScanner() int {
 	close(ports)
 	close(results)
 	sort.Ints(openports)
+	sort.Ints(closedports)
 
 	//TODO 5 : Enhance the output for easier consumption, include closed ports
 
 	for _, port := range openports {
-		fmt.Printf("%d open\n", port)
+		fmt.Printf("%d,open\n", port)	// Changed to comma separated value format
 	}
+	for _, port := range closedports {
+		fmt.Printf("%d,closed\n", port)
+	}
+	finished := time.Now()	// To help tune for TODO 4
+	fmt.Printf("Time elapsed: %v\n", finished.Sub(start))	// To help tune for TODO 4
 
-	return len(openports) // TODO 6 : Return total number of ports scanned (number open, number closed); 
+	return len(openports), len(closedports) // TODO 6 : Return total number of ports scanned (number open, number closed); 
 	//you'll have to modify the function parameter list in the defintion and the values in the scanner_test
 }
--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -7,19 +7,20 @@ import (
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
 func TestOpenPort(t *testing.T){
 
-    got := PortScanner() // Currently function returns only number of open ports
+    numOpen, _ := PortScanner() // Function returns both number of open and number of closed ports
     want := 2 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
 	          //consider what would happen if you parameterize the portscanner address and ports to scan
 
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
+    if numOpen != want {
+        t.Errorf("got %d, wanted %d", numOpen, want)
     }
 }
 
 func TestTotalPortsScanned(t *testing.T){
 	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
 
-    got := PortScanner() // Currently function returns only number of open ports
+    numOpen, numClosed := PortScanner() // Functions returns both number of open and number of closed ports
+	got := numOpen + numClosed
     want := 1024 // default value; consider what would happen if you parameterize the portscanner ports to scan
 
     if got != want {
